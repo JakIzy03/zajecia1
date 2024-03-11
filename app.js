489a18674ddbc0124e7418c9eb8289036e163906
JakIzy03 <lapucynek@gmail.com>
2024-03-11 19:38:26 +0100

    add file

--- app.js
+++ app.js
@@ -1,20 +1,3 @@
-const http = require("http");
-
-const PORT = 3000;
-
-function requestListener(request, response){
-    response.setHeader("Content-Type", "text/html");
-    response.write("<html>");
-    response.write("<head><title>My first Node App</title></head>"); 
-    response.write("<body><h1>Hello, World!</h1><body>");
-    response.write("</body>");
-    response.end;
-    console.log("RequestListener", {
-        request,
-        response,
-    });
-}
-
-const server = http.createServer(requestListener);
-
-server.listen(PORT);
+const FULLNAME = "Jakub Izydorczyk";
+const STUDENT_ID = 42881;
+console.log(`My name is${FULLNAME}. Mystudent ID is ${STUDENT_ID}`);
